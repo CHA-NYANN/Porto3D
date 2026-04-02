8b62f6ed8419186a7d1071c3d281ec44e86ae2ba
CHA-NYANN <patrahapsari@gmail.com>
2026-04-02 12:57:15 +0700

    first commit

--- js/i18n.js
+++ js/i18n.js
@@ -69,7 +69,7 @@ const translations = {
         'contact.intro': 'Tertarik berkolaborasi atau punya proyek menarik? Jangan ragu untuk menghubungi saya!',
         
         // Footer
-        'footer.note': 'Dibuat dengan 💜 dan banyak kopi',
+        'footer.note': 'Dibuat dengan <svg aria-hidden="true" viewBox="0 0 24 24" width="14" height="14" style="display:inline-block;vertical-align:-0.15em;"><path fill="currentColor" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg> dan banyak kopi',
         
         // Artwork Page
         'artwork.title': 'GALERI ARTWORK',
